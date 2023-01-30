ff19473b3824468fd0fb6d40c98a5d5eb29ac046
valentin-llv <lelievre.valentin17@gmail.com>
2023-01-30 08:45:48 +0100

    Debug test

--- src/WikiChallenge-proxy.js
+++ src/WikiChallenge-proxy.js
@@ -20,6 +20,8 @@ https.createServer(
     serverOptions,
 
     (request, response) => {
+    console.log(request.headers.origin)
+
     // Check if request method is different than GET
     if(request.method != "GET") {
         response.writeHead(200, {
@@ -44,7 +46,7 @@ https.createServer(
     try {
         requestUrl = new URL("https://testwikichallenge.com" + request.url); // Change later
     } catch(error) {
-        writeHead200(response, request)
+        writeHead200(response, request);
         response.end("Url is not valid.");
         return false;
     }
@@ -52,7 +54,7 @@ https.createServer(
     // Check if requested url params are present
     let requestQuery = requestUrl.searchParams.get("query");
     if(requestQuery === null) {
-        writeHead200(response, request)
+        writeHead200(response, request);
         response.end("Request query is not defined.");
         return false;
     }
@@ -62,14 +64,14 @@ https.createServer(
     try {
         queryUrl = new URL(requestQuery);
     } catch(error) {
-        writeHead200(response, request)
+        writeHead200(response, request);
         response.end("Query url is not valid.");
         return false;
     }
 
     // Check if given url match the allowed external ressources
     if(!allowedExtrnalRessources.includes(queryUrl.hostname)) {
-        writeHead200(response, request)
+        writeHead200(response, request);
         response.end("Query url hostname is not allowed.");
         return false;
     }
@@ -78,11 +80,11 @@ https.createServer(
     fetch(requestQuery).then((data) => {
         return data.text();
     }).then((data) => {
-        writeHead200(response, request)
+        writeHead200(response, request);
         response.end(JSON.stringify({ response: data }));
         return false;
     }).catch(() => {
-        writeHead200(response, request)
+        writeHead200(response, request);
         response.end(JSON.stringify({ response: false }));
         return false;
     });
